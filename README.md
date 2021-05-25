5f9a00f52763d49d6833d1c858f1b9b1eb1f7b94
risto <risto.kowaczewski@gmail.com>
2021-05-25 22:47:06 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tracardi plugin sdk
+
+This is the SDK for task plugins in TRACARDI Customer Data Platform
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tracardi_plugin_sdk',
+    version='0.1',
+    description='Tracardi Plugin SDK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Risto Kowaczewski',
+    author_email='risto.kowaczewski@gmail.com',
+    packages=['tracardi_plugin_sdk'],
+    install_requires=[
+        'pydantic'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
