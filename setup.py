478561f9c53cbd747abfcc1439ea2424d5b87c6e
risto <risto.kowaczewski@gmail.com>
2021-08-29 00:43:42 +0200

    console log

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tracardi_plugin_sdk',
-    version='0.1.27',
+    version='0.6.1',
     description='Tracardi Plugin SDK',
     long_description=long_description,
     long_description_content_type="text/markdown",
--- tracardi_plugin_sdk/domain/console.py
+++ tracardi_plugin_sdk/domain/console.py
@@ -1,18 +1,37 @@
-from typing import List, Any
-
-from pydantic import BaseModel
+from typing import Tuple
 
 
-class Console(BaseModel):
-    logs: List[Any] = []
-    errors: List[Any] = []
-    warnings: List[Any] = []
+class Log:
 
-    def log(self, item):
-        self.logs.append(item)
+    def __init__(self, module, class_name, type, message):
+        self.message = message
+        self.type = type
+        self.class_name = class_name
+        self.module = module
 
-    def error(self, item):
+    def __repr__(self):
+        return f"class: {self.class_name}, module: {self.module}, type: {self.type}, message: {self.message}"
+
+
+class Console:
+
+    def __init__(self, class_name, module):
+        self.module = module
+        self.class_name = class_name
+        self.infos = []
+        self.errors = []
+        self.warnings = []
+
+    def log(self, item: str):
+        self.infos.append(item)
+
+    def error(self, item: str):
         self.errors.append(item)
 
-    def warning(self, item):
+    def warning(self, item: str):
         self.warnings.append(item)
+
+    def get_logs(self) -> Tuple[Log]:
+        for type, logs in zip(["info", "warning", "error"], [self.infos, self.warnings, self.errors]):
+            for message in logs:
+                yield Log(self.module, self.class_name, type, message)
