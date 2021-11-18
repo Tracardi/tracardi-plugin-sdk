0198da2f2d819585aea719ca53d8793ac7004a3e
risto <risto.kowaczewski@gmail.com>
2021-11-18 23:53:23 +0100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tracardi_plugin_sdk',
-    version='0.6.25',
+    version='0.6.26',
     description='Tracardi Plugin SDK',
     long_description=long_description,
     long_description_content_type="text/markdown",
