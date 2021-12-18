91640e513e97c9ddda8628a66cf096e5ad0f2972
risto <risto.kowaczewski@gmail.com>
2021-12-18 17:05:15 +0100

    Append, Join Payload.

--- tracardi_plugin_sdk/domain/result.py
+++ tracardi_plugin_sdk/domain/result.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 from pydantic import BaseModel
 
 
@@ -8,6 +8,18 @@ class Result(BaseModel):
 
     class Config:
         allow_mutation = False
+
+    @staticmethod
+    def append_input(result: 'Result', payload) -> Optional['Result']:
+        if result is None:
+            return None
+        elif result.value is None:
+            return Result(value=payload, port=result.port)
+        elif isinstance(result.value, dict):
+            payload.update(result.value)
+            return Result(value=payload, port=result.port)
+        else:
+            return result
 
 
 class MissingResult(Result):
